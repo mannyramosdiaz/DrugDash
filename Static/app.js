b70ba9c9cca63747efc989103afdcf606f9d3341
BradBarrett <65555921+BradBarrett@users.noreply.github.com>
2020-10-08 20:55:41 -0400

    update

--- Static/app.js
+++ Static/app.js
@@ -1,18 +1,147 @@
 
+
+function Info() {
+    //select drop down information
+    var selector = d3.select("#selDataset");
+    
+    d3.csv("../csv Data/states.csv").then((data) => {
+
+        //console.log(data)
+
+        var stateNames = data;
+
+        stateNames.forEach((state) => {
+            console.log(state.state)
+            selector
+            .append("option")
+            .text(state.state)
+            .property("value", state.state);
+        });
+
+        var fvalue = stateNames[0];
+
+        pieCharts2(fvalue);
+        lineCharts2(fvalue);
+
+    });
+}
+
+Info();
+
+function pieCharts2(state) {
+    let pieLoc = "https://cors-anywhere.herokuapp.com/https://calm-fortress-78674.herokuapp.com/api/v1.0/drugdata"
+    //let bubbleLoc = "/api/v1.0/drugdata"
+
+    d3.json(pieLoc).then((response) => {
+        console.log(JSON.stringify(response));
+        var states = response;
+
+        var resultsArray = states.filter(stateobj => stateobj.state == state && stateobj.year == 2018);
+        if (resultsArray.length > 0) {
+            var result =resultsArray[0];
+            console.log(result)
+            console.log(JSON.stringify(result));
+            var values = [result.coke_od, result.her_od, result.opi_od];
+            
+            
+            let pie = {
+                values: values,
+                labels: ['Cocain OD', 'Heroin OD', 'Non-Heroin Opiod OD'],
+                type: 'pie' 
+            };
+    
+            let pieChart = [pie];
+            var layoutpie = {
+                height: 400,
+                width: 400
+            };
+    
+            Plotly.newPlot("piePlot",pieChart, layoutpie)
+            }
+        else { 
+
+        }
+
+        
+    })
+}
+
+function lineCharts2(state) {
+    let lineLoc = "https://cors-anywhere.herokuapp.com/https://calm-fortress-78674.herokuapp.com/api/v1.0/drugvunemployment"
+    //let bubbleLoc = "/api/v1.0/drugdata"
+
+    d3.json(lineLoc).then((response) => {
+        console.log(response);
+
+        var states = response;
+
+        var resultsArray = states.filter(stateobj => stateobj.state == state);
+        resultsArray.sort((state1, state2) => state1.year - state2.year)
+
+        
+
+        
+        var yearArray = [];
+        var unempArray = [];
+        var odrateArray = [];
+
+        resultsArray.forEach(stateobj => {
+            yearArray.push(stateobj.year)
+            unempArray.push(stateobj.unemployment_rate)
+            odrateArray.push(stateobj.OD_perctage)
+        })
+
+        var Unemployeement = {
+            x: yearArray,
+            y: unempArray,
+            type: 'scatter',
+            name: 'Unemployment Rate'
+        };
+
+        var ODRate = {
+            x: yearArray,
+            y: odrateArray,
+            type: 'scatter',
+            name: 'OD Rate % to total Deaths'
+        };
+
+        var layout = {
+            title: "Unemployment vs OD Rate"
+        }
+
+        var data = [Unemployeement, ODRate];
+        console.log(JSON.stringify(data,null,2))
+        Plotly.newPlot("linePlot", data, layout)
+        
+    })
+}
+
 function bubbleCharts() {
     let bubbleLoc = "https://cors-anywhere.herokuapp.com/https://calm-fortress-78674.herokuapp.com/api/v1.0/drugdata"
     //let bubbleLoc = "/api/v1.0/drugdata"
 
-    d3.json(bubbleLoc).then(function(response){
+    d3.json(bubbleLoc).then((response) => {
         //console.log(response);
 
-        var states = response.state;
-        var totod = response.OD_sum;
-        var years = response.year;
+        var states = response;
+
+        var resultsArray = states.filter(stateobj => stateobj.state == state);
+        resultsArray.sort((state1, state2) => state1.year - state2.year)
+
+        
+        var yearArray = [];
+        var unempArray = [];
+        var odrateArray = [];
+
+        resultsArray.forEach(stateobj => {
+            yearArray.push(stateobj.year)
+            unempArray.push(stateobj.unemployment_rate)
+            odrateArray.push(stateobj.OD_perctage)
+        })
 
         let bubble = {
             x: ["Alabama", "California", "Arizona", "Virginia"],
-            y: ["2015", "2016", "2017", "2018"],
+            y: yearArray,
             mode: "markers",
             marker: {
                 size: [5,6,7,8,9],
@@ -33,158 +162,7 @@ function bubbleCharts() {
 };
 
 
-bubbleCharts();
-
-
-
-pieCharts();
-
-function pieCharts() {
-    let bubbleLoc = "https://cors-anywhere.herokuapp.com/https://calm-fortress-78674.herokuapp.com/api/v1.0/drugdata"
-    //let bubbleLoc = "/api/v1.0/drugdata"
-
-    d3.json(bubbleLoc).then(function(response){
-        //console.log(response);
-
-        var test = [5,9,10]
-        
-        let pie = {
-            values: test,
-            labels: ['Five', 'Nine', 'Ten'],
-            type: 'pie',
-            
-        };
-
-        let pieChart = [pie];
-        var layoutpie = {
-            height: 400,
-            width: 400,
-        };
-
-        Plotly.newPlot("piePlot",pieChart, layoutpie)
-        
-    })
-};
-
-
-pieCharts2();
-function pieCharts2(state) {
-    let pieLoc = "https://cors-anywhere.herokuapp.com/https://calm-fortress-78674.herokuapp.com/api/v1.0/drugdata"
-    //let bubbleLoc = "/api/v1.0/drugdata"
-
-    d3.json(pieLoc).then((response) => {
-        console.log(response);
-        var states = response.state;
-
-        var resultsArray = states.filter(stateobj => stateobj.state == state);
-        var results =resultsArray[0];
-        var values = [results.coke_od, results.her_od, results.opi_od];
-        
-        
-        let pie = {
-            values: values,
-            labels: ['Cocain OD', 'Herion OD', 'Non-Herion Opiod OD'],
-            type: 'pie' 
-            
-        };
-
-        let pieChart = [pie];
-        var layoutpie = {
-            height: 600,
-            width: 600,
-        };
-
-        Plotly.newPlot("piePlot2",pieChart, layoutpie)
-        
-    })
-}
-
-lineCharts();
-
-function lineCharts() {
-    let lineLoc = "https://cors-anywhere.herokuapp.com/https://calm-fortress-78674.herokuapp.com/api/v1.0/drugvunemployment"
-    //let bubbleLoc = "/api/v1.0/drugdata"
-
-    d3.json(lineLoc).then(function(response){
-        //console.log(response);
-
-        var trace1 = {
-            x: [1, 2, 3,4],
-            y: [10, 15, 13, 17],
-            type: 'scatter'
-        };
-
-        var trace2 = {
-            x: [1, 2, 3,4],
-            y: [16, 5, 11, 9],
-            type: 'scatter'
-        };
-
-        var data = [trace1, trace2];
-
-        Plotly.newPlot("linePlot", data)
-        
-    })
-};
-
-lineCharts2();
-
-function lineCharts2(state) {
-    let lineLoc = "https://cors-anywhere.herokuapp.com/https://calm-fortress-78674.herokuapp.com/api/v1.0/drugvunemployment"
-    //let bubbleLoc = "/api/v1.0/drugdata"
-
-    d3.json(lineLoc).then((response) => {
-        console.log(response);
-
-        var states = response.state;
-
-        var resultsArray = states.filter(stateobj => stateobj.state == state);
-        var results =resultsArray[0];
-        var years = results.year;
-        var unemp = results.unemployment_rate;
-        var odrate = results.OD_perctage;
-
-        var Unemployeement = {
-            x: [years],
-            y: [unemp],
-            type: 'scatter'
-        };
-
-        var ODRate = {
-            x: [years],
-            y: [odrate],
-            type: 'scatter'
-        };
-
-        var data = [Unemployeement, ODRate];
-
-        Plotly.newPlot("linePlot2", data)
-        
-    })
-};
-
-function Info() {
-    //select drop down information
-    var selector = d3.select("#selDataset");
-    // pull sample numbers from json
-        var stateNames = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columnbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachuset", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" ];
-        console.log(stateNames);
-        //populate drop down items
-        stateNames.forEach((state) => {
-            selector
-                .append("option")
-                .text(state)
-                .property("value", state);
-        })
-        // set first number in sample as starter
-        var fvalue = stateNames[0];
-        lineCharts2(fvalue);
-        pieCharts2(fvalue);
-};
-
 function optionChanged(newstate) {
-    lineCharts2(newstate);
     pieCharts2(newstate);
-};
-
-Info();
+    lineCharts2(newstate);
+}
